0291fa95c22f1d55a7da2b88bd00c7cf401ae9ee
EvanCode20 <evan.teague11@gmail.com>
2023-06-02 17:55:38 +0200

    Added login and register backend

--- app/src/main/java/com/jesd_opsc_poe/chrono/RegisterActivity.kt
+++ app/src/main/java/com/jesd_opsc_poe/chrono/RegisterActivity.kt
@@ -1,14 +1,22 @@
 package com.jesd_opsc_poe.chrono
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var tvLogIn: TextView
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -20,5 +28,37 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        auth = Firebase.auth
+
+        val txtRegisterEmail = findViewById<TextInputEditText>(R.id.txtRegisterEmail)
+        val txtRegisterPassword = findViewById<TextInputEditText>(R.id.txtRegisterPassword)
+        val btnRegister = findViewById<AppCompatButton>(R.id.btnRegister)
+
+        btnRegister.setOnClickListener(){
+            val email = txtRegisterEmail.text.toString()
+            val password = txtRegisterPassword.text.toString()
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+
+                    }
+                }
+        }
+
+
+
+
+
     }
 }
