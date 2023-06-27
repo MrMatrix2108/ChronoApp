e888fd4c4e57db85ee26d0134022009a22f3ead6
EvanCode20 <evan.teague11@gmail.com>
2023-06-27 19:11:36 +0200

    added funs to calc date and totals

--- app/src/main/java/com/jesd_opsc_poe/chrono/GraphActivity.kt
+++ app/src/main/java/com/jesd_opsc_poe/chrono/GraphActivity.kt
@@ -18,6 +18,13 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.threeten.bp.format.DateTimeFormatter
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.threeten.bp.LocalDate
+
+import java.util.Date
 
 class GraphActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -25,8 +32,8 @@ class GraphActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graph)
         auth = Firebase.auth
+        AndroidThreeTen.init(this)
 
-        val tvTasks = findViewById<TextView>(R.id.tvNotTaks)
         val totalHrsEntries : MutableList<Entry> = mutableListOf()
 
         var currentUser = auth.currentUser
@@ -38,99 +45,132 @@ class GraphActivity : AppCompatActivity() {
         }
         val dbTasksref = FirebaseDatabase.getInstance().getReference("Tasks")
         var allTasks : HashMap<String, Task>?
-        dbTasksref.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                totalHrsEntries.clear()
-                allTasks = snapshot.getValue<HashMap<String, Task>>()
-                val userMap : Map<String, Task>? = allTasks?.filterValues { it.userKey == userKey}
-                val todayMap : Map<String, Task>? = userMap?.filterValues { it.date == "2023-06-06" }
-                tvTasks.text = totalHrsEntries.toString()
 
-            }
+                dbTasksref.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        totalHrsEntries.clear()
+                        allTasks = snapshot.getValue<HashMap<String, Task>>()
+                        val userMap: Map<String, Task>? =
+                            allTasks?.filterValues { it.userKey == userKey }
+                        val todayMap: Map<String, Task>? =
+                            userMap?.filterValues { it.date == "2023-06-06" }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("fail", "onCancelled : ${error.toException()}")
-            }
-        })
+                        var startDate = "2023-06-01"
+                        var endDate = "2023-06-30"
+                        var dates : List<String> = getDatesInRange(startDate, endDate)
+                        var dailyTotals : MutableList<DailyTotal> = mutableListOf<DailyTotal>()
+                        for(date in dates){
+                            dailyTotals.add(calculateDailyTotalHours(userMap, date))
+                        }
+                        if (userMap != null) {
+                            var i: Float = 1F
+                            for (dailyTotal in dailyTotals) {
+
+                                totalHrsEntries.add(Entry(i, dailyTotal.time))
+                                i++
+                            }
+
+                            val lineChart: LineChart = findViewById(R.id.lineChart)
+                            //this is the list of entries for the minimum hours goal line graph
+                            val minGoalEntries = listOf(
+                                Entry(1f, 2f),
+                                Entry(2f, 5f),
+                                Entry(3f, 8f),
+                                Entry(4f, 4f),
+                                Entry(5f, 3f),
+                                Entry(6f, 1f),
+                                Entry(7f, 8f),
+                                Entry(8f, 10f),
+                                Entry(9f, 15f),
+                                Entry(10f, 8f)
+                            )
+
+                            //this is the list of entries for the maximum hours goal line graph
+                            val maxGoalEntries = listOf(
+                                Entry(1f, 3f),
+                                Entry(2f, 6f),
+                                Entry(3f, 4f),
+                                Entry(4f, 9f),
+                                Entry(5f, 12f),
+                                Entry(6f, 3f),
+                                Entry(7f, 5f),
+                                Entry(8f, 11f),
+                                Entry(9f, 14f),
+                                Entry(10f, 9f)
+                            )
+
+                            val totalHrsDataset = LineDataSet(totalHrsEntries, "Total")
+                            totalHrsDataset.color = Color.RED // Set the line color
+                            totalHrsDataset.setDrawCircles(true)
+                            totalHrsDataset.setDrawValues(false)
+
+                            val minGoalDataset = LineDataSet(minGoalEntries, "Min Goal")
+                            minGoalDataset.color = Color.BLUE // Set the line color
+                            minGoalDataset.setDrawCircles(true)
+                            minGoalDataset.setDrawValues(false)
+
+                            val maxGoalDataset = LineDataSet(maxGoalEntries, "Max Goal")
+                            maxGoalDataset.color = Color.BLACK // Set the line color
+                            maxGoalDataset.setDrawCircles(true)
+                            maxGoalDataset.setDrawValues(false)
+
+                            val graphData = LineData(totalHrsDataset)
+                            lineChart.data = graphData
+
+                            val xAxisLine: XAxis = lineChart.xAxis
+                            xAxisLine.position = XAxis.XAxisPosition.BOTTOM // Set X Axis position
+                            xAxisLine.setDrawGridLines(false)
+                            val yAxisLine: YAxis = lineChart.axisLeft
+                            yAxisLine.setDrawGridLines(false)
+                            lineChart.axisRight.isEnabled = false
+
+                            val description: Description = lineChart.description
+                            description.text = ""
+
+                            lineChart.invalidate()
+
+                        }
 
 
+                    }
 
-        val lineChart: LineChart = findViewById(R.id.lineChart)
-
-        //this is the list of entries for the total hours line graph
-
-//            Entry(1f, 7f),
-//            Entry(2f, 12f),
-//            Entry(3f, 10f),
-//            Entry(4f, 15f),
-//            Entry(5f, 8f),
-//            Entry(6f, 15f),
-//            Entry(7f, 8f),
-//            Entry(8f, 10f),
-//            Entry(9f, 15f),
-//            Entry(10f, 8f)
-//        )
-
-        //this is the list of entries for the minimum hours goal line graph
-        val minGoalEntries = listOf(
-            Entry(1f, 2f),
-            Entry(2f, 5f),
-            Entry(3f, 8f),
-            Entry(4f, 4f),
-            Entry(5f, 3f),
-            Entry(6f, 1f),
-            Entry(7f, 8f),
-            Entry(8f, 10f),
-            Entry(9f, 15f),
-            Entry(10f, 8f)
-        )
-
-        //this is the list of entries for the maximum hours goal line graph
-        val maxGoalEntries = listOf(
-            Entry(1f, 3f),
-            Entry(2f, 6f),
-            Entry(3f, 4f),
-            Entry(4f, 9f),
-            Entry(5f, 12f),
-            Entry(6f, 3f),
-            Entry(7f, 5f),
-            Entry(8f, 11f),
-            Entry(9f, 14f),
-            Entry(10f, 9f)
-        )
-
-        val totalHrsDataset = LineDataSet(totalHrsEntries, "Total")
-        totalHrsDataset.color = Color.RED // Set the line color
-        totalHrsDataset.setDrawCircles(true)
-        totalHrsDataset.setDrawValues(false)
-
-        val minGoalDataset = LineDataSet(minGoalEntries, "Min Goal")
-        minGoalDataset.color = Color.BLUE // Set the line color
-        minGoalDataset.setDrawCircles(true)
-        minGoalDataset.setDrawValues(false)
-
-        val maxGoalDataset = LineDataSet(maxGoalEntries, "Max Goal")
-        maxGoalDataset.color = Color.BLACK // Set the line color
-        maxGoalDataset.setDrawCircles(true)
-        maxGoalDataset.setDrawValues(false)
-
-        val graphData = LineData(totalHrsDataset,minGoalDataset,maxGoalDataset)
-        lineChart.data = graphData
-
-        val xAxisLine: XAxis = lineChart.xAxis
-        xAxisLine.position = XAxis.XAxisPosition.BOTTOM // Set X Axis position
-        xAxisLine.setDrawGridLines(false)
-        val yAxisLine: YAxis = lineChart.axisLeft
-        yAxisLine.setDrawGridLines(false)
-        lineChart.axisRight.isEnabled = false
-
-        val description: Description = lineChart.description
-        description.text = ""
-
-        lineChart.invalidate()
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.e("fail", "onCancelled : ${error.toException()}")
+                    }
+                })
     }
 
+    private fun convertTimeToFloat(time: String?): Float {
+        val parts = time?.split(":")
+        val hours = parts?.get(0)?.toInt()
+        val minutes = parts?.get(1)?.toInt()
+        val floatTime = hours?.plus(((minutes?.toFloat() ?: ) / 60))
+        return floatTime
+    }
 
+    private fun calculateDailyTotalHours(tasks: Map<String, Task>?, desiredDate: String?): DailyTotal {
+        var totalHours = 0f
+        if (tasks != null) {
+            for (task in tasks.values) {
+                if (task.date == desiredDate) {
+                    totalHours += convertTimeToFloat(task.duration)
+                }
+            }
+        }
 
+        return DailyTotal(totalHours, desiredDate)
+    }
 
+    fun getDatesInRange(startDate: String, lastDate: String): List<String> {
+        val dates = mutableListOf<String>()
+        var currentDate = LocalDate.parse(startDate)
+        var endDate = LocalDate.parse(lastDate)
+
+        while (!currentDate.isAfter(endDate)) {
+            dates.add(currentDate.format(DateTimeFormatter.ISO_LOCAL_DATE))
+            currentDate = currentDate.plusDays(1)
+        }
+
+        return dates
+    }
 }
